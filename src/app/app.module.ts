8008234359c915783206c2d971f563220c334b12
CarlosLuz <carlosluz12892@gmail.com>
2024-01-22 08:43:30 -0300

    add dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar'; // add uma toolbar para ser usada na interface html
 import {MatButtonModule} from '@angular/material/button'; // add um button para ser usado na interface html
 import {MatIconModule} from '@angular/material/icon'; // add um icon para ser usado na interface html
+import { MatCardModule } from "@angular/material/card";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -19,7 +24,8 @@ import { MessagesComponent } from './messages/messages.component';
     AppComponent,
     HeroesComponent, // add component heroes
     HeroDetailComponent, // add component hero details
-    MessagesComponent // add component messages
+    MessagesComponent, // add component messages
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +34,11 @@ import { MessagesComponent } from './messages/messages.component';
     MatToolbarModule, // add from materials
     MatButtonModule, // add from materials
     MatIconModule, // add from materials
-    MatExpansionModule
+    MatCardModule,
+    MatExpansionModule,
+    FlexLayoutModule,
+    MatTooltipModule,
+    AppRoutingModule // add from terminal ng g app-routing --flat --module=app
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,5 +5,8 @@ import { Hero } from "./hero.model"; // modelo do array a ser seguido
 export const HEROES: Hero[] = [
   {id: 1, name: 'Cyclops', power: 'Laser eyes'},
   {id: 2, name: 'Deadpool', power: 'Immortality'},
-  {id: 3, name: 'Ghost Rider', power: 'Look of Penance'}
+  {id: 3, name: 'Ghost Rider', power: 'Look of Penance'},
+  {id: 4, name: 'Aquaman', power: ' Telepathically of the sea'},
+  {id: 5, name: 'Watchmen', power: 'Null'},
+  {id: 6, name: 'Iron fist', power: 'Specialist of CHI'}
 ]
