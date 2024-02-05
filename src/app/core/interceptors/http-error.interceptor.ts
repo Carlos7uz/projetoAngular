ef8292c7e633b093e1dceb59190518c1af3c74ba
CarlosLuz <carlosluz12892@gmail.com>
2024-02-05 10:59:17 -0300

    dnv esse banco sla tmb

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -8,6 +8,7 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -17,14 +18,22 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
-        console.log(err)
+        if(!environment.production){
+          console.log(err)
+        }
+
 
         let errorMsg = '';
 
         if (err.error instanceof ErrorEvent) {
           errorMsg = `Error ${err.error.message}`;
+
         } else if (Array.isArray(err.error) && err.error.length){
-            errorMsg = `Error: ${err.error[0]}`
+            errorMsg = `Error: ${err.error[0]}`;
+
+        } else if (err.error.errors) {
+            errorMsg = `Error: ${err.error.errors}`;
+
         } else {
             errorMsg = `Error Code: ${err.status}, Message ${err.message}`;
         }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: '/api'
+  baseUrl: 'https://curso-tour-of-heroes-api.herokuapp.com/api'
 };
